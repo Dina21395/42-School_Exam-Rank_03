d24ecd1f9a54b0d3825a34342afdec0ffad69f48
Dina Youssef ElGammal <dos_21395@hotmail.com>
2024-10-30 07:28:20 +0400

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,10 +1,4 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+#include "get_next_line.h"
 
 char * ft_str_dup(char * str)
 {
